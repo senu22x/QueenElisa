78095f735b7583447fbf4b09f8feaa9de7ebdb3c
senu22x <senudisanjana58@gmail.com>
2024-11-08 19:01:47 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: 'mongodb+srv://yrk975org648official:EsHK24WOCDM0tAuA@cluster0.kfedz.mongodb.net/', 
+  MONGODB_URL: 'mongodb://mongo:kKXgyRpBDDuUHjWBDREmFAjIazcXYDos@autorack.proxy.rlwy.net:25071', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
